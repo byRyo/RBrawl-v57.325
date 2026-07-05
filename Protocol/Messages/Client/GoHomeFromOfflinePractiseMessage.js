4cd5ae17653461c4c42fbff17efa4b3591a73725
Ryo <rrblx511@gmail.com>
2026-07-06 00:28:02 +0200

    Create GoHomeFromOfflinePractiseMessage.js

---
+++ Protocol/Messages/Client/GoHomeFromOfflinePractiseMessage.js
@@ -0,0 +1,21 @@
+const PiranhaMessage = require('../../PiranhaMessage')
+const OwnHomeDataMessage = require('../Server/OwnHomeDataMessage')
+
+class GoHomeFromOfflinePractiseMessage extends PiranhaMessage {
+  constructor (bytes, client) {
+    super(bytes)
+    this.client = client
+    this.id = 14109
+    this.version = 0
+  }
+
+  async decode () {
+    this.readBoolean()
+  }
+
+  async process () {
+    new OwnHomeDataMessage(this.client).send()
+  }
+}
+
+module.exports = GoHomeFromOfflinePractiseMessage
