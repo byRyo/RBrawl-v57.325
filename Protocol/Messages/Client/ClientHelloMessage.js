e6103a47cafbcce7c5e77973068e723a06271e07
Ryo <rrblx511@gmail.com>
2026-07-06 00:12:45 +0200

    Create ClientHelloMessage.js

---
+++ Protocol/Messages/Client/ClientHelloMessage.js
@@ -0,0 +1,31 @@
+const PiranhaMessage = require('../../PiranhaMessage')
+const ServerHelloMessage = require('../Server/ServerHelloMessage')
+
+class ClientHelloMessage extends PiranhaMessage {
+  constructor (bytes, client) {
+    super(bytes)
+    this.client = client
+    this.id = 10100
+    this.version = 0
+  }
+
+  async decode () {
+    this.data = {}
+    this.data.Protocol = this.readInt()
+    this.data.KeyVersion = this.readInt()
+    this.data.MajorVersion = this.readInt()
+    this.data.MinorVersion = this.readInt()
+    this.data.Build = this.readInt()
+    this.data.ContentHash = this.readString()
+    this.data.DeviceType = this.readInt()
+    this.data.AppStore = this.readInt()
+
+    console.log(this.data)
+  }
+
+  async process () {
+    new ServerHelloMessage(this.client).send()
+  }
+}
+
+module.exports = ClientHelloMessage
